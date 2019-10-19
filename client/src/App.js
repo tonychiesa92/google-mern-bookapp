71e0010973d1dd19d588c133e9ce6fe09023e903
Tony C <tonychiesa92@gmail.com>
2019-10-19 16:35:56 -0400

    search books is working fully. Saved books are not showing up in /saved

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Saved from "./pages/Saved";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SaveBooks from "./pages/SaveBooks";
+import SearchBooks from "./pages/SearchBooks";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer"
+import "./App.css"
 
-import Search from "./pages/Search";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <Router>
       <div>
-        <Navbar />
-        <Wrapper>
-          <Route exact path="/" component={Search} />
-          <Route exact path="/saved" component={Saved} />
-        </Wrapper>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={SearchBooks} />
+          <Route exact path="/saved" component={SaveBooks} />
+          <Route exact path="/saved/:id" component={SaveBooks} />
+          <Route component={NoMatch} /> 
+        </Switch>
         <Footer />
       </div>
     </Router>
--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,33 +1,25 @@
 import React from "react";
 import "./style.css";
 
-// Using the datalist element we can create autofill suggestions based on the props.breeds array
-function SearchForm(props) {
-  return (
-    <form className="search">
-      <div className="form-group">
-        <label htmlFor="breed">Breed Name:</label>
-        <input
-          value={props.search}
-          onChange={props.handleInputChange}
-          name="breed"
-          list="breeds"
-          type="text"
-          className="form-control"
-          placeholder="Type in a dog breed to begin"
-          id="breed"
-        />
-        <datalist id="breeds">
-          {props.breeds.map(breed => (
-            <option value={breed} key={breed} />
-          ))}
-        </datalist>
-        <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
-          Search
-        </button>
-      </div>
-    </form>
-  );
+const SearchForm = props => {
+    return (
+        <form>
+            <div className="form-group">
+                <label className="BookSearchLabel"><h3>Search For Book</h3></label>
+                <br></br>
+                <input className="col-12 form-control"
+                    value={props.search}
+                    type="text"
+                    name="searchBook"
+                    placeholder="Enter Book's Name"
+                    onChange={props.handleInputChange}
+                />
+            </div>
+            <button type="submit" className="submitBtn btn btn-primary" onClick={props.handleFormSubmit}>
+                Submit
+            </button>
+        </form>
+    )
 }
 
-export default SearchForm;
+export default SearchForm
