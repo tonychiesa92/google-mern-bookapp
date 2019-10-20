77f9ded758cf4ed1f2def38986e7ec5e05df10bc
Tony C <tonychiesa92@gmail.com>
2019-10-19 20:17:52 -0400

    updated for heroku.

---
+++ client/build/precache-manifest.bcae9ee8e5d56f28a0ed2297d608223e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "04512222f14a8e66d6a744c635a9522e",
+    "url": "/static/media/bookshelf.04512222.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "345c80891cfa38c2d109",
+    "url": "/static/js/main.4ab55d1c.chunk.js"
+  },
+  {
+    "revision": "a41f707d8e2eaf0456e7",
+    "url": "/static/js/2.750a9378.chunk.js"
+  },
+  {
+    "revision": "345c80891cfa38c2d109",
+    "url": "/static/css/main.bd210db4.chunk.css"
+  },
+  {
+    "revision": "ecf2a07b425dc305e48a97716668d809",
+    "url": "/index.html"
+  }
+];
