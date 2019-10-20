b2eacb93b6fa25491020590e3d180a8954c0ed67
Tony C <tonychiesa92@gmail.com>
2019-10-19 20:39:12 -0400

    updating build

---
+++ client/build/precache-manifest.87482bd180cb54d91f35c4fb79edb768.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d6b6bb575beaf20f349afb9d14176c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66952776f3b2161bccdf",
+    "url": "/static/css/main.b594fee1.chunk.css"
+  },
+  {
+    "revision": "41e740c249b496d99b32",
+    "url": "/static/js/2.a125c973.chunk.js"
+  },
+  {
+    "revision": "66952776f3b2161bccdf",
+    "url": "/static/js/main.f48c58a9.chunk.js"
+  },
+  {
+    "revision": "55855338982e403cd313",
+    "url": "/static/js/runtime-main.d80f8818.js"
+  },
+  {
+    "revision": "04512222f14a8e66d6a744c635a9522e",
+    "url": "/static/media/bookshelf.04512222.jpg"
+  }
+]);
